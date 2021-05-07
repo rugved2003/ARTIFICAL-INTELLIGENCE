b9ceed659d878bdf445e8131c07d56aeeaceaaf3
rugved2003 <81752288+rugved2003@users.noreply.github.com>
2021-05-07 18:43:51 +0530

    To print positive numbers in a range
    python program to print positive numbers in a range

---
+++ postive_number1.py
@@ -0,0 +1,23 @@
+#python program to print positive numbers in a range
+print("list1 = [12,-7,5,64,-14]")
+list1 = [12,-7,5,64,-14]
+for num in list1:
+    if num >= 0:
+        print(num, end = " " )
+print(" \n")
+print("list2 = [12,14,-95,3]")
+
+list2 = [12,14,-95,3]
+for num in list2:
+    if num >= 0:
+        print(num, end = " " )
+        
+        
+        
+        
+ output:
+list1 = [12,-7,5,64,-14]
+12 5 64  
+
+list2 = [12,14,-95,3]
+12 14 3
